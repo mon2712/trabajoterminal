7e2f06a416da8a4b7c0dbd3e45ee9aa932dfb267
Montse Mendoza <montse@Montses-MacBook-Air.local>
2018-02-20 17:08:00 -0600

    Correccion de identacion

--- app/components/app.js
+++ app/components/app.js
@@ -42,6 +42,6 @@ class App extends React.Component {
             </Router>    
         );
     }
-    }
+}
 
 export default App;
--- app/components/login/main.js
+++ app/components/login/main.js
@@ -5,19 +5,19 @@ import InputPass from './inputPass';
 class Login extends React.Component {
     
     render() {
-        console.log("Llego al login");
-       return (
-        <div className='loginContainer'>
-            <span id='title'>Bienvenidos</span>
-            <span id='instruction'>Ingresa tu usuario y contraseña</span>
-            <InputUser />
-            <InputPass />
-            <div className='button'>
-                Aceptar
-            </div>
-           <span id='forgot'>¿Olvidaste tu contraseña?</span>
-        </div>
-       );
-    }
+		console.log("Llego al login");
+		return (
+			<div className='loginContainer'>
+				<span id='title'>Bienvenidos</span>
+				<span id='instruction'>Ingresa tu usuario y contraseña</span>
+				<InputUser />
+				<InputPass />
+				<div className='button'>
+					Aceptar
+				</div>
+				<span id='forgot'>¿Olvidaste tu contraseña?</span>
+			</div>
+		);
+	}
   }
   export default Login;
