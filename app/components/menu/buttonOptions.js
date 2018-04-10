3cd45515bd1b5d98cd66f28eb407e64d10869f25
montse <montse94@gmail.com>
2018-04-10 01:19:57 -0500

    Cambios para redireccion o popUp

--- app/components/menu/buttonOptions.js
+++ app/components/menu/buttonOptions.js
@@ -1,8 +1,25 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { BrowserRouter as Redirect} from 'react-router-dom';
+
 class ButtonOptions extends React.Component {
-
-
+    constructor(props){
+        super(props);
+        this.sendAction=this.sendAction.bind(this);
+        this.statePopUp=this.statePopUp.bind(this);
+        this.state = {
+            popUpActive: false
+        };  
+    }
+    sendAction(){
+        console.log("soy de path y redirecciono");
+        //this.setState({popUpActive: false});
+        //this.props.actions.getUserLogin("esme","esme");
+        <Redirect to='/vistaCentro' />
+    }
+    statePopUp(){
+        console.log("activo el popUp")
+        this.setState({popUpActive: true});
+    }
     renderButtons(){
         //map de menuTypes
         var menuTypes = this.props.store.menuTypes[this.props.type];
@@ -10,12 +27,10 @@ class ButtonOptions extends React.Component {
           return null;
 
         return menuTypes.map((opt, index) => (
-            <Link key={index} to={opt.path}>
-              <div className='buttonContainer' key={index} style={{background: opt.color, border: opt.color} }>
-                  <span className={"ico "+opt.ico}></span>
-                  <span>{opt.text}</span>
-              </div>
-            </Link>
+            <div className='buttonContainer' key={index} onClick={opt.popUp === true ? this.statePopUp : () => this.sendAction(opt.path)} style={{background: opt.color, border: opt.color}}>
+                <span className={"ico "+opt.ico}></span>
+                <span>{opt.text}</span>
+            </div>
         ));
     }
 
--- app/fillData/menuTypes.js
+++ app/fillData/menuTypes.js
@@ -4,63 +4,75 @@
                 "ico": "icon-clipboard",
                 "text": "Asistencia",
                 "path": "/asistencia",
-                "color": "#4765dd"
+                "color": "#4765dd",
+                "popUp": false
             },{
                 "ico": "icon-learning",
                 "text": "Vista Centro",
                 "path": "/vistaCentro",
-                "color": "#ebc531"
+                "color": "#ebc531",
+                "popUp": false
             },{
                 "ico": "icon-payment",
                 "text": "Registrar Pago",
                 "path": "/registrarPago",
-                "color": "#e84eb2"
+                "color": "#e84eb2",
+                "popUp": true
             },{
                 "ico": "icon-calls",
                 "text": "Lista Llamadas",
                 "path": "/llamadasPendientes",
-                "color": "#6ddfc1"
+                "color": "#6ddfc1",
+                "popUp": false
             },{
                 "ico": "icon-point-of-service",
                 "text": "Lista Adeudo",
                 "path": "/paymentList",
-                "color": "#acea49"
+                "color": "#acea49",
+                "popUp": false
             },{
                 "ico": "icon-minute-mark",
                 "text": "Tiempo Reducido",
                 "path": "/tiempoReducido",
-                "color": "#48a2bb"
+                "color": "#48a2bb",
+                "popUp": false
             }],
             "instructor": [{
                 "ico": "icon-update",
                 "text": "Actualizar Reporte",
                 "path": "/",
-                "color": "#4765dd"
+                "color": "#4765dd",
+                "popUp": true
             },{
                 "ico": "icon-learning",
                 "text": "Vista Centro",
                 "path": "/",
-                "color": "#ebc531"
+                "color": "#ebc531",
+                "popUp": false
             },{
                 "ico": "icon-team",
                 "text": "Registro Asistentes",
                 "path": "/",
-                "color": "#814bea"
+                "color": "#814bea",
+                "popUp": false
             },{
                 "ico": "icon-calls",
                 "text": "Lista Llamadas",
                 "path": "/llamadasPendientes",
-                "color": "#6ddfc1"
+                "color": "#6ddfc1",
+                "popUp": false
             },{
                 "ico": "icon-point-of-service",
                 "text": "Lista Adeudos",
                 "path": "/",
-                "color": "#acea49"
+                "color": "#acea49",
+                "popUp": false
             },{
                 "ico": "icon-printer",
                 "text": "Imprimir",
                 "path": "/",
-                "color": "#e9414f"
+                "color": "#e9414f",
+                "popUp": true
             }]
         }
     }
