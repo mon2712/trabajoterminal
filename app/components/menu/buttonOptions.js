d12eb33fb9eaf7edf78e924533d8df03e8c3ba51
montse <montse94@gmail.com>
2018-04-11 18:02:01 -0500

    Correccion para los popUps y correccion de css

--- app/components/menu/buttonOptions.js
+++ app/components/menu/buttonOptions.js
@@ -1,24 +1,35 @@
 import React from 'react';
+import PrintOptions from './printOptions';
 import { BrowserRouter as Router, Switch, Route, Link, Redirect, withRouter } from 'react-router-dom';
 
 class ButtonOptions extends React.Component {
     constructor(props){
         super(props);
         this.sendAction=this.sendAction.bind(this);
+        this.closePopUp=this.closePopUp.bind(this);
         this.state = {
-            popUpActive: false
+            popUpActive: false,
+            id: ""
         };  
     }
-    sendAction(){
-        console.log("activo el popUp")
-        this.setState({popUpActive: true});
+    sendAction(id){
+        this.setState({
+            popUpActive: true,
+            id: id
+        });
+    }
+    closePopUp(){
+        this.setState({
+            popUpActive: false,
+            id: ""
+        });
     }
     renderButtons(){
         var menuTypes = this.props.store.menuTypes[this.props.type];
        
         return menuTypes.map((opt, index) => (
             opt.popUp === true ?
-            <div className='buttonContainer' key={index} onClick={this.sendAction} style={{background: opt.color, border: opt.color}}>
+            <div className='buttonContainer' key={index} onClick={() => this.sendAction(opt.id)} style={{background: opt.color, border: opt.color}}>
                 <span className={"ico "+opt.ico}></span>
                 <span>{opt.text}</span>
             </div>
@@ -35,7 +46,12 @@ class ButtonOptions extends React.Component {
     render() {
 		return (
             <div className="optionsContainer">
-              {this.props.store.menuTypes[this.props.type] != undefined ? this.renderButtons() : null}
+                {this.props.store.menuTypes[this.props.type] != undefined ? this.renderButtons() : null}
+                {
+                    this.state.popUpActive === true && this.state.id===2 ? 
+                        <PrintOptions closePopUp={()=>this.closePopUp}/> 
+                    : null
+                }                
             </div>
 		);
 	}
